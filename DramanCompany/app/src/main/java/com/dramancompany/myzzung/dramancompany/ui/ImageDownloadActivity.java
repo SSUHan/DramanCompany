8f7db6573bd152f5d630ba073db96c4dbf17b261
이준수 <ljs93kr@gmail.com>
2016-03-01 00:29:18 +0900

    ImageView noimage 사진 배치

--- DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/ui/ImageDownloadActivity.java
+++ DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/ui/ImageDownloadActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.dramancompany.myzzung.dramancompany.R;
 
@@ -17,6 +18,10 @@ public class ImageDownloadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_image_download);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
+
+    }
+
+    private void GetImageByUrl(String image_url, ImageView imageView){
 
     }
 
