94c4784b827a493c51ee76d8caa4b349e274aedf
이준수 <ljs93kr@gmail.com>
2016-03-01 01:55:59 +0900

    helper 동작완료 , 하나의 task 는 한번만 실행가능

--- DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/helper/ImageDownHelper.java
+++ DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/helper/ImageDownHelper.java
@@ -1,29 +1,72 @@
 package com.dramancompany.myzzung.dramancompany.helper;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by myZZUNG on 2016. 3. 1..
  */
-public class ImageDownHelper extends AsyncTask {
+public class ImageDownHelper extends AsyncTask<String, Integer, Bitmap> {
+
+    private String TAG = "ImageDownHelper";
 
     private String mImageUrl;
     private ImageView mImageView;
+    private Context mContext;
 
     public ImageDownHelper(){
 
     }
 
-    public ImageDownHelper(String image_url, ImageView imageView){
+    public ImageDownHelper(Context context, String image_url, ImageView imageView){
+        mContext = context;
         mImageUrl = image_url;
         mImageView = imageView;
     }
+
     @Override
-    protected Bitmap doInBackground(Object[] params) {
-        return null;
+    protected Bitmap doInBackground(String... params) {
+        Bitmap bitmap = null;
+        try{
+            URL url = new URL(mImageUrl);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.connect();
+
+            int cSize = conn.getContentLength();
+            Log.d(TAG, "cSize : " + cSize);
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream(),cSize);
+            bitmap = BitmapFactory.decodeStream(bis);
+            bis.close();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 
-    
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+
+        if(bitmap == null){
+            Toast.makeText(mContext, "Bitmap이 null입니다", Toast.LENGTH_SHORT).show();
+
+        }else{
+            mImageView.setImageBitmap(bitmap);
+        }
+    }
 }
--- DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/ui/ImageDownloadActivity.java
+++ DramanCompany/app/src/main/java/com/dramancompany/myzzung/dramancompany/ui/ImageDownloadActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.dramancompany.myzzung.dramancompany.R;
+import com.dramancompany.myzzung.dramancompany.helper.ImageDownHelper;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
@@ -30,6 +31,7 @@ public class ImageDownloadActivity extends AppCompatActivity {
     private ImageView imageview3;
 
     private String image_url;
+    private ImageDownHelper imageHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,47 +45,24 @@ public class ImageDownloadActivity extends AppCompatActivity {
         imageview3 = (ImageView)findViewById(R.id.imageview_3);
 
         image_url = "http://ljs93kr.cafe24.com/mysky.png";
+        imageHelper = new ImageDownHelper(getApplicationContext(),image_url,imageview1);
 
     }
 
     public void doImageBtn1(View v){
-        GetImageByUrl(image_url,imageview1);
+        imageHelper.execute();
     }
 
     public void doImageBtn2(View v){
-        GetImageByUrl(image_url,imageview2);
+        //GetImageByUrl(image_url,imageview2);
     }
 
     public void doImageBtn3(View v){
-        GetImageByUrl(image_url,imageview3);
+        //GetImageByUrl(image_url,imageview3);
     }
 
-    private Bitmap GetImageByUrl(String image_url, ImageView imageView){
-        Bitmap bitmap = null;
-        try{
-            URL url = new URL(image_url);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
-            conn.connect();
-
-            int cSize = conn.getContentLength();
-            Log.d(TAG,"cSize : "+cSize);
-            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream(),cSize);
-            bitmap = BitmapFactory.decodeStream(bis);
-            bis.close();
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        if(bitmap == null){
-            Toast.makeText(ImageDownloadActivity.this, "Bitmap이 null입니다", Toast.LENGTH_SHORT).show();
-            return null;
-        }else{
-            imageView.setImageBitmap(bitmap);
-            return bitmap;
-        }
-    }
+//    private Bitmap GetImageByUrl(String image_url, ImageView imageView){
+//
+//    }
 
 }
